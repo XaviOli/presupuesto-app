20d9824bbd9e9ae36b378a0a25aea1420d3984f3
Javier Diaz <javier.diazoliviero@gmail.com>
2022-10-20 22:48:15 +0200

    Onclick

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
+import { useState } from 'react';
 import { Form, H2, Label } from './components/Styles';
 
+// --------- enum ---------
+
+const PRICES = {
+  WEB: 500,
+  SEO: 300,
+  GOOGLE: 200
+}
+
+// -------------------------
+
+const getFinalPrice = (event, finalPrice, setFinalPrice) => {
+  const servicePrice = Number(event.target.value);
+
+  if(event.target.checked) {
+    setFinalPrice(finalPrice + servicePrice)
+  } else {
+    setFinalPrice(finalPrice - servicePrice)
+  }
+
+}
+
 function App() {
+
+  const [ finalPrice, setFinalPrice ] = useState(0);
+
   return (
     <div className="App">
       <H2>¿Qué quieres hacer?</H2>
@@ -9,23 +34,35 @@ function App() {
       <Form>
         <Label>
           <input 
+            onClick={(event) => getFinalPrice(event, finalPrice, setFinalPrice)}
             type='checkbox' 
-            name='web' />
-          {' '}Una página web(500€)
+            name='web'
+            value={PRICES.WEB}
+            />
+              {' '}Una página web({PRICES.WEB}€)
+        </Label>
+        <Label>
+          <input
+            onClick={(event) => getFinalPrice(event, finalPrice, setFinalPrice)}
+            type='checkbox' 
+            name='web'
+            value={PRICES.SEO}
+             />
+              {' '}Una consultoria SEO({PRICES.SEO}€)
         </Label>
         <Label>
           <input 
+            onClick={(event) => getFinalPrice(event, finalPrice, setFinalPrice)}
             type='checkbox' 
-            name='web' />
-          {' '}Una consultoria SEO(300€)
-        </Label>
-        <Label>
-          <input 
-            type='checkbox' 
-            name='web' />
-          {' '}Una campaña de Google Ads(200€)
+            name='web' 
+            value={PRICES.GOOGLE}
+            />
+              {' '}Una campaña de Google Ads({PRICES.GOOGLE}€)
         </Label>
       </Form>
+
+      <p>Precio: {finalPrice}</p>
+
     </div>
   );
 }
