99a152471b5b7b80e63c397e9e7adebc8998af29
Javier Diaz <javier.diazoliviero@gmail.com>
2022-10-20 21:44:06 +0200

    New component for styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import './App.css';
-import styled from 'styled-components';
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  width: fit-content;
-  margin: auto;
-`;
-
-const H2 = styled.h2`
-  font-size: 1rem;
-  font-weight: normal;
-  padding-bottom: 1rem;
-`;
-
-const Label = styled.label`
-  padding-bottom: 1rem;
-`
+import { Form, H2, Label } from './components/Styles';
 
 function App() {
   return (
---
+++ src/components/Styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: fit-content;
+  margin: auto;
+`;
+
+export const H2 = styled.h2`
+  font-size: 1rem;
+  font-weight: normal;
+  padding-bottom: 1rem;
+`;
+
+export const Label = styled.label`
+  padding-bottom: 1rem;
+`;
